8951519eb7a3ea48d4d8b959026ee416ce59ac71
Madhav4b1 <madhavarao.nelluri1999@gmail.com>
2021-08-30 14:04:30 -0500

    assignment11

---
+++ src/main/java/Assignment11/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment11;
+
+/**
+ *
+ * @author s545525
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=35,b=5;
+        System.out.print("sum="+(a+b)+", difference="+(a-b)+", multiplication="+(a*b)+", division="+(a/b));
+        
+        // TODO code application logic here
+    }
+    
+}
